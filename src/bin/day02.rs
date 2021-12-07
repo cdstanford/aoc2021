acb7c5fb03ff8cba16d153c72fbcc7e72f386322
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-12-07 18:16:25 -0500

    day 2 cosmetic improvements

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -3,20 +3,32 @@
 */
 
 use aoc2021::util;
+use std::iter::FromIterator;
 
 #[derive(Debug)]
 enum Command {
     Fwd(usize),
     Down(usize),
     Up(usize),
+    AimDown(usize),
+    AimUp(usize),
 }
 impl Command {
-    fn parse(raw: &str) -> Self {
+    fn parse_pt1(raw: &str) -> Self {
         let (name, val): (String, usize) = util::split_pair_parsed(raw);
         match name.as_str() {
             "forward" => Self::Fwd(val),
             "down" => Self::Down(val),
             "up" => Self::Up(val),
+            _ => panic!("Failed to parse command: {}", raw),
+        }
+    }
+    fn parse_pt2(raw: &str) -> Self {
+        let (name, val): (String, usize) = util::split_pair_parsed(raw);
+        match name.as_str() {
+            "forward" => Self::Fwd(val),
+            "down" => Self::AimDown(val),
+            "up" => Self::AimUp(val),
             _ => panic!("Failed to parse command: {}", raw),
         }
     }
@@ -34,53 +46,38 @@ impl Default for Submarine {
     }
 }
 impl Submarine {
-    fn new() -> Self {
-        Default::default()
-    }
-    fn execute_pt1(&mut self, comm: &Command) {
-        match comm {
-            Command::Fwd(i) => self.xpos += i,
-            Command::Down(i) => self.depth += i,
-            Command::Up(i) => self.depth -= i,
-        }
-    }
-    fn execute_pt2(&mut self, comm: &Command) {
+    fn execute(&mut self, comm: &Command) {
         match comm {
             Command::Fwd(i) => {
                 self.xpos += i;
                 self.depth += self.aim * i;
             }
-            Command::Down(i) => {
-                self.aim += i;
-            }
-            Command::Up(i) => {
-                self.aim -= i;
-            }
+            Command::Down(i) => self.depth += i,
+            Command::Up(i) => self.depth -= i,
+            Command::AimDown(i) => self.aim += i,
+            Command::AimUp(i) => self.aim -= i,
         }
+    }
+    fn answer(&self) -> usize {
+        self.xpos * self.depth
+    }
+}
+impl FromIterator<Command> for Submarine {
+    fn from_iter<I: IntoIterator<Item = Command>>(iter: I) -> Self {
+        let mut sub: Submarine = Default::default();
+        println!("-> Submarine initial state: {:?}", sub);
+        for comm in iter {
+            sub.execute(&comm);
+        }
+        println!("-> Submarine final state: {:?}", sub);
+        sub
     }
 }
 
 fn main() {
-    let instrs: Vec<Command> = util::file_to_vec("input/day02.txt")
-        .iter()
-        .map(|x| Command::parse(x))
-        .collect();
-    {
-        let mut sub = Submarine::new();
-        println!("[Part 1] Initial state: {:?}", sub);
-        for instr in instrs.iter() {
-            sub.execute_pt1(instr);
-        }
-        println!("[Part 1] End state: {:?}", sub);
-        println!("[Part 1] Answer: {}", sub.xpos * sub.depth)
-    }
-    {
-        let mut sub = Submarine::new();
-        println!("[Part 2] Initial state: {:?}", sub);
-        for instr in instrs.iter() {
-            sub.execute_pt2(instr);
-        }
-        println!("[Part 2] End state: {:?}", sub);
-        println!("[Part 2] Answer: {}", sub.xpos * sub.depth)
-    }
+    let raw = util::file_to_vec("input/day02.txt");
+    let pt1: Submarine = raw.iter().map(|x| Command::parse_pt1(x)).collect();
+    println!("Part 1 answer: {}", pt1.answer());
+    let pt2: Submarine = raw.iter().map(|x| Command::parse_pt2(x)).collect();
+    println!("Part 2 answer: {}", pt2.answer());
 }
