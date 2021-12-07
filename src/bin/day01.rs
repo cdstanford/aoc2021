38193c02254701ebac9f52bb9d59f9f5f8b91793
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-12-06 21:54:11 -0500

    day 1 pt 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,40 +1,58 @@
+/*
+    Advent of Code 2021 Day 1
+*/
+
 use aoc2021::util;
+use std::convert::TryInto;
+use std::fmt::Debug;
 
 /*
-    Iterator over adjacent pairs
+    Iterator over N-tuples
+    Note: requires N >= 1
 */
-struct PairIter<T, I: Iterator<Item = T>> {
-    prev: Option<T>,
+struct TupleIter<T, I: Iterator<Item = T>, const N: usize> {
+    prev: Vec<T>, // vector of strictly less than N items
     sub: I,
 }
-impl<T, I> Iterator for PairIter<T, I>
+impl<T, I, const N: usize> Iterator for TupleIter<T, I, N>
 where
-    T: Copy,
+    T: Copy + Debug,
     I: Iterator<Item = T>,
 {
-    type Item = (T, T);
+    type Item = [T; N];
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.prev.is_none() {
-            self.prev = Some(self.sub.next()?);
+        while self.prev.len() + 1 < N {
+            self.prev.push(self.sub.next()?);
         }
-        let t1 = self.prev.unwrap();
-        let t2 = self.sub.next()?;
-        self.prev = Some(t2);
-        Some((t1, t2))
+        debug_assert_eq!(self.prev.len() + 1, N);
+        let mut result = Vec::new();
+        for &i in &self.prev {
+            result.push(i);
+        }
+        let new = self.sub.next()?;
+        result.push(new);
+        self.prev.push(new);
+        self.prev.remove(0);
+        debug_assert_eq!(result.len(), N);
+        Some(result.try_into().unwrap())
     }
 }
 
-fn adjacencies<T, I>(iter: I) -> impl Iterator<Item = (T, T)>
+fn window<T, I, const N: usize>(iter: I) -> impl Iterator<Item = [T; N]>
 where
-    T: Copy,
+    T: Copy + Debug,
     I: Iterator<Item = T>,
 {
-    PairIter { prev: None, sub: iter }
+    TupleIter { prev: vec![], sub: iter }
 }
 
 fn main() {
     let depths: Vec<usize> = util::file_to_vec_parsed("input/day01.txt");
-    let result = adjacencies(depths.iter()).filter(|(d1, d2)| d1 < d2).count();
-    println!("Part 1 answer: {}", result);
+    let pt1 = window(depths.iter()).filter(|[d1, d2]| d1 < d2).count();
+    println!("Part 1 answer: {}", pt1);
+    let pt2 = window(depths.iter())
+        .filter(|&[d1, d2, d3, d4]| d1 + d2 + d3 < d2 + d3 + d4)
+        .count();
+    println!("Part 2 answer: {}", pt2);
 }
